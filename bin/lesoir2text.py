24f1a79763c8174a5e29f2a4d78859a47f03f140
Alexandre Dulaunoy <a@foo.be>
2011-07-18 21:29:17 +0200

    Output logs to STDERR

--- bin/lesoir2text.py
+++ bin/lesoir2text.py
@@ -1,25 +1,34 @@
-
+#!/usr/bin/python
+#
 # lesoir.be website sucks with all the advertising and
-# if you want to read in your Kindle, this is a disaster.
+# if you want to read on your Kindle, this is a disaster.
 #
 # The following script fetch the most read article from lesoir.be
-# and make an ascii text-file to read with some comfort the news on your Kindle.
+# (from their official sitemap) and make an ascii text file of
+# all the articles with the junk removed.
 #
+# lesoir2text uses standard Python libraries + lynx for the text conversion
+#
+# The script is in the public domain.
+#
+# Author: Alexandre Dulaunoy - http://www.foo.be/
+#
+# You can use sendKindle to send it directly to your Kindle
+# https://github.com/kparal/sendKindle
+#
+# To use it : python lesoir2text.py >lesoir-`date +%Y%m%M`.txt
 
 from lxml import etree
 import urllib2
 import os.path
 from urlparse import urlparse
 import re
-#import BeautifulSoup
-# Non core modules
-# 'easy_install stripogram'
-#from stripogram import html2text
-#import html2text
+import sys
 
+# require lynx
 import TextFormat
 
-max = 3
+max = 15
 toplesoir = "http://www.lesoir.be/sitemap-mostread.xml"
 
 sock = urllib2.urlopen(toplesoir)
@@ -52,12 +61,12 @@ for parent in tree.getiterator():
                 break
             if child.tag == '{http://www.google.com/schemas/sitemap/0.84}loc':
                 i=i+1
-                print "Fetching... "+child.text
+                sys.stderr.write("Fetching... "+child.text)
                 x=urlparse(child.text)
                 path = os.path.split(x[2])
                 article=FetchPage(child.text)
                 x = TextFormat.TextFormatter()
                 txtarticle = x.html2text( article )
+                print "----\n"
                 print RemoveJunk(txtarticle[0])
-
 sock.close()
